ea736d0178b9217719abee604895ac2fb336c3ca
Tim Gesell <timothy.gesell@bvsd.org>
2025-08-20 20:27:54 +0000

    Added a menu option.

--- Conversions.java
+++ Conversions.java
@@ -1,16 +1,41 @@
-// Tim Gesell
-// 08/20/25
-
+import java.util.Scanner;
+/**
+ * This application converts between various units of measurement.
+ *
+ * @author Tim Gesell
+ * @version 8/20/2025
+ */
 public class Conversions
 {
     public static void main(String[] args)
     {
-        System.out.println("1. Fahrenheit to Celsius");
-        System.out.println("2. Celsius to Fahrenheit");
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("1. Celsius to Fahrenheit");
+        System.out.println("2. Fahrenheit to Celsius");
         System.out.println("3. Feet to Meters");
         System.out.println("4. Meters to Feet");
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
+        System.out.println("7. Miles to Feet");
+        //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
+        //You can add even more if you would like.
 
+        int selection = keyboard.nextInt();
+        keyboard.nextLine();  //go past the end-line and be ready for more input
+
+        if (selection == 1)
+        {
+            System.out.println("Enter Celsius: ");
+            double celsius = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+        }
+        if (selection == 2)
+        {
+            //add the code to ask the user to enter Fahrenheit and then convert to Celsius
+        }
+        //add more if statements here.
     }
 }
