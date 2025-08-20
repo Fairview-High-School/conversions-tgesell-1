f0ba80a8e639fc1c1635b3e4275f2b8d703c9a4b
Tim Gesell <timothy.gesell@bvsd.org>
2025-08-20 18:56:41 +0000

    Added menu

--- Conversions.java
+++ Conversions.java
@@ -1,1 +1,16 @@
+// Tim Gesell
+// 08/20/25
 
+public class Conversions
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.println("3. Feet to Meters");
+        System.out.println("4. Meters to Feet");
+        System.out.println("5. Ounces to Milliliters");
+        System.out.println("6. Milliliters to Ounces");
+
+    }
+}
